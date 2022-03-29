1c4f78fc88fba0c2cca617810c04e00d42f64c38
VitaL <vitaldukm@gmail.com>
2022-03-29 18:19:37 +0100

    CH2 complete

--- Exercise Files/Ch 2/interface_start.py
+++ Exercise Files/Ch 2/interface_start.py
@@ -3,6 +3,11 @@
 
 from abc import ABC, abstractmethod
 
+class JSONify(ABC):
+    @abstractmethod
+    def toJSON(self):
+        pass
+
 
 class GraphicShape(ABC):
     def __init__(self):
@@ -13,13 +18,17 @@ class GraphicShape(ABC):
         pass
 
 
-class Circle(GraphicShape):
+class Circle(GraphicShape, JSONify):
     def __init__(self, radius):
         self.radius = radius
 
     def calcArea(self):
         return 3.14 * (self.radius ** 2)
 
+    def toJSON(self):
+        return f"{{ \"circle\": {str(self.calcArea())} }}"
 
+        
 c = Circle(10)
 print(c.calcArea())
+print(c.toJSON())
